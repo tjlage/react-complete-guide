167d46551533013ecc7984561dcb567f2bbb9c16
Tom Lage <tjlage1@gmail.com>
2021-12-11 00:58:32 -0500

    Example of only allowing 1 root element

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,14 @@
 function ExpenseItem() {
-  return <h2>Expense item!</h2>;
+  return (
+    //   Only 1 root(div) element in return statement
+    <div>
+      <div>March 28th 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
+    </div>
+  );
 }
 // or
 
