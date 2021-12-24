d776cc56dc73f46fd35969db1604a1cc1e5fe875
Tom <tjlage1@gmail.com>
2021-12-24 00:10:55 -0500

    Created an expenses component to display expense items

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
-    { title: 'e1',
+    { 
+      id: 'e1',
+      title: 'Car Insurance',
       date: new Date(2021, 2, 28),
       amount: 294.67},
-    { title: 'e2',
+    { 
+      id: 'e2',
+      title: 'Gas',
       date: new Date(2021, 3, 28),
       amount: 468.67
     },
-    { title: 'e3',
+    { 
+      id: 'e3',
+      title: 'Groceries',
       date: new Date(2021, 4, 28),
       amount: 324.67
     }
@@ -17,10 +23,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Lets get started!</h2>
-        <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/> {/* or <ExpenseItem></ExpenseItem> */}
-        <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
-        <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
+        <Expenses expenses={expenses} />
       </header>
     </div>
   );
---
+++ src/components/Expenses.js
@@ -0,0 +1,13 @@
+import './Expenses.css'
+import ExpenseItem from './ExpenseItem';
+
+function Expenses(props){
+    return(
+        <div className="expenses">
+            <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date}/> {/* or <ExpenseItem></ExpenseItem> */}
+            <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date}/>
+            <ExpenseItem title={props.expenses[2].title} amount={props.expenses[2].amount} date={props.expenses[2].date}/>
+        </div>
+    );
+}
+export default Expenses;
