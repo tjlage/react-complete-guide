2fc0cf3f9f5f5fe1ef964fdd1080817c925aed96
Tom <tjlage1@gmail.com>
2021-12-23 23:41:26 -0500

    Split date into a standalone component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,11 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString('en-US', { month: 'long' });
-  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
-  const year = props.date.getFullYear();
-  //Above logic could be placed directly in JSX but this is preferred
+  //Removed date logic and put it into its own separate component
   return (
     //   Only 1 root(div) element in return statement
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
