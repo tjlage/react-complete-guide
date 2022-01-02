34a4463f1477f543ffafd717a204cef4507d2382
Tom <tjlage1@gmail.com>
2022-01-02 18:07:30 -0500

    Updated title with useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,21 @@
+import { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
   //Removed date logic and put it into its own separate component
+  const [title, setTitle] = useState(props.title);
+  //useState requires the varibles default value as arg. Then it returns the var name and updating function
   const clickHandler = () =>{
-    console.log("Clicked");
+    setTitle('Updated!!!')
   };
   return (
     //   Only 1 root(div) element in return statement
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
