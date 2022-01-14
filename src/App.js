462b3c741823e7eaf9a450ec6291f847210df162
Tom Lage <tjlage1@gmail.com>
2022-01-13 22:21:22 -0500

    Passing functions between child and parent

--- src/App.js
+++ src/App.js
@@ -2,33 +2,39 @@ import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
+  const expensesHandler = () => {};
   const expenses = [
-    { 
-      id: 'e1',
-      title: 'Car Insurance',
+    {
+      id: "e1",
+      title: "Car Insurance",
       date: new Date(2021, 2, 28),
-      amount: 294.67},
-    { 
-      id: 'e2',
-      title: 'Gas',
-      date: new Date(2021, 3, 28),
-      amount: 468.67
+      amount: 294.67,
     },
-    { 
-      id: 'e3',
-      title: 'Groceries',
+    {
+      id: "e2",
+      title: "Gas",
+      date: new Date(2021, 3, 28),
+      amount: 468.67,
+    },
+    {
+      id: "e3",
+      title: "Groceries",
       date: new Date(2021, 4, 28),
-      amount: 324.67
-    }
+      amount: 324.67,
+    },
   ];
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expense);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <NewExpense />
+        <NewExpense onAddExpense={addExpenseHandler} />
         <Expenses expenses={expenses} />
       </header>
     </div>
   );
-}
+};
 
 export default App; //default export
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,9 +1,18 @@
-import './NewExpense.css';
-import ExpenseForm from './ExpenseForm';
-const NewExpense = () => {
-    return (<div className='new-expense'>
-        <ExpenseForm />
-    </div>);
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+  };
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    </div>
+  );
 };
 
 export default NewExpense;
