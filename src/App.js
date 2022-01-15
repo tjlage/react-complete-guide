69c6fec007c8c259fb01cc5401fb439f24ca1fd5
Tom Lage <tjlage1@gmail.com>
2022-01-15 02:04:35 -0500

    Mapping lists dynamically

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+import { useState } from "react";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Car Insurance",
+    date: new Date(2021, 2, 28),
+    amount: 294.67,
+  },
+  {
+    id: "e2",
+    title: "Gas",
+    date: new Date(2021, 3, 28),
+    amount: 468.67,
+  },
+  {
+    id: "e3",
+    title: "Groceries",
+    date: new Date(2021, 4, 28),
+    amount: 324.67,
+  },
+];
 
 const App = () => {
-  const expensesHandler = () => {};
-  const expenses = [
-    {
-      id: "e1",
-      title: "Car Insurance",
-      date: new Date(2021, 2, 28),
-      amount: 294.67,
-    },
-    {
-      id: "e2",
-      title: "Gas",
-      date: new Date(2021, 3, 28),
-      amount: 468.67,
-    },
-    {
-      id: "e3",
-      title: "Groceries",
-      date: new Date(2021, 4, 28),
-      amount: 324.67,
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
   return (
     <div className="App">
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,22 +13,14 @@ const Expenses = (props) => {
     <div>
       <Card className="expenses">
         <ExpensesFilter filter={filteredYear} selected={selectedYear} />
-        <ExpenseItem
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}
-        />{" "}
-        {/* or <ExpenseItem></ExpenseItem> */}
-        <ExpenseItem
-          title={props.expenses[1].title}
-          amount={props.expenses[1].amount}
-          date={props.expenses[1].date}
-        />
-        <ExpenseItem
-          title={props.expenses[2].title}
-          amount={props.expenses[2].amount}
-          date={props.expenses[2].date}
-        />
+        {props.expenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
