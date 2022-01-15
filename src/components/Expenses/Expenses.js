2dccad9b36a1cb1cea31d0abde4475d13b66437a
Tom Lage <tjlage1@gmail.com>
2022-01-15 17:25:43 -0500

    Conditional JSX output

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,18 +12,34 @@ const Expenses = (props) => {
   const filteredExpenses = props.expenses.filter((expense) => {
     return expense.date.getFullYear().toString() === selectedYear;
   });
+
+  // Alternative to inline JSX
+  // let expensesContent = <p>No Expenses Found</p>;
+
+  // if (filteredExpenses.length > 0) {
+  //   expensesContent = filteredExpenses.map((expense) => (
+  //     <ExpenseItem
+  //       key={expense.id}
+  //       title={expense.title}
+  //       amount={expense.amount}
+  //       date={expense.date}
+  //     />
+  //   ));
+  // }
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter filter={filteredYear} selected={selectedYear} />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {filteredExpenses.length === 0 && <p>No Expenses Found</p>}
+        {filteredExpenses.length > 0 &&
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))}
       </Card>
     </div>
   );
